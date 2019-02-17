68fe5394885b1168d606ff8fdeba01ba25a787e2
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-02-17 15:18:52 -0500

    Finished a redesign and updated content/projects

---
+++ app.js
@@ -0,0 +1,18 @@
+M.AutoInit();
+
+// var instance = M.Carousel.init({
+//     fullWidth: true,
+//     indicators: true,
+
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+    
+//     var instances = M.Carousel.init(elems, options);
+//   });
+
+// $(".carousel").carousel({
+//     fullWidth: false,
+//     indicators: true
+// });
